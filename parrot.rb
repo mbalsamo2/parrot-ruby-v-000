9f87775c65cd283daa0ef2013d39541ddc367e27
Maria Balsamo <mia.balsamo@gmail.com>
2017-12-18 20:01:23 +0000

    automatically backed up by learn

--- parrot.rb
+++ parrot.rb
@@ -1,6 +1,6 @@
 # Create method `parrot` that outputs a given phrase and
 # returns the phrase
 def parrot(string = "Squak!")
-  puts parrot()
-  return parrot()
+  puts "#{string}"
+  return "#{string}"
 end
